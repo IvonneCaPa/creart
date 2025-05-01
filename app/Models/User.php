0d7bb4ce49b42beb1b046011eec8ad2430ab0241
Ivonne Cantor <ivonnecp@gmail.com>
2025-05-01 17:33:51 +0200

    agregando mutadores y accesores, en Model\user falta el resto

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -34,15 +34,20 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    protected function name(): Attribute 
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return new Attribute(
+            set: fn($value) => strtolower($value),
+            get: fn ($value) => ucwords($value)
+        );
     }
 }
